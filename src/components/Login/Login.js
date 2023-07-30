4f4676c4c0bbaaaf473a5a709b5103f6ee45f899
Армен Погосян <armen-pogosyan@mail.ru>
2023-07-31 02:59:17 +0300

    Верстка_1.3

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,20 @@
 import AuthForm from '../AuthForm/AuthForm';
+import {useFormAndValidation} from '../../hooks/useFormAndValidation'
 
 function Login () {
+  const {values, handleChange, errors} = useFormAndValidation({
+    email: "",
+    password: ""
+ });
   return (
     <>
-      <AuthForm title="Рады видеть!" buttonText = "Войти"/>
+      <AuthForm 
+      title="Рады видеть!" 
+      buttonText = "Войти"
+      values={values}
+      handleChange={handleChange}
+      errors={errors}
+      />
       
     </>
   );
